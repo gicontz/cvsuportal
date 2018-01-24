a4c8072aceb8e59eec4950b3811b97be5179ee74
Erwin <33570485+xrwiz@users.noreply.github.com>
2018-01-24 18:50:08 +0800

    login loading

--- js/login.js
+++ js/login.js
@@ -18,16 +18,29 @@ $(".btn-modals").click(function(){
 
 
 function loginCheck(){
+
+	$("#login-loading").show();
+    $("#btn-login-login").hide();
+    $('.alert').hide();
+
 	var data = new Object();
 	data["request_type"] = "request-login";
 	data["username"] = $("#btn-login-username").val();
 	data["password"] = $("#btn-login-password").val();
 
     $.post("lib/login.php", {data: data}, function(callback){
-        if(callback == "GOTO PROFILE"){
-        	window.open("profile-instructor.php", '_self');
+        if(callback == "GOTO PROFILE instructor"){
+        	window.open("profile-instructor", '_self');
+        }else if (callback == "GOTO PROFILE deptchair") {
+            window.open("profile-deptchair", '_self');
+        }else if (callback == "GOTO PROFILE student") {
+            window.open("profile-student", '_self');
+        }else if (callback == "GOTO PROFILE admin") {
+            window.open("profile-admin", '_self');
         }else{
+            $("#login-loading").hide();
             $('.alert').show();
+            $("#btn-login-login").show();
         }
     });
 }
